d99a510420645a1485ac94f2101592926ca3c365
Simon lauzeral <sim78100@hotmail.fr>
2020-04-11 19:15:20 -0400

    ouverture d'une image dans une autre activité avec possibilité d'écrire un message

--- app/src/main/java/com/somestudents/steganographapp/FullImageActivity.java
+++ app/src/main/java/com/somestudents/steganographapp/FullImageActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.MediaTimestamp;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -17,12 +18,9 @@ public class FullImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_image);
-
-        Intent i = new Intent();
+        Intent i = getIntent();
         int position = i.getExtras().getInt("id");
-
         ImageAdapter adapter = new ImageAdapter(this);
-
         imageView = (ImageView) findViewById(R.id.fullImage);
         imageView.setImageResource(adapter.images[position]);
 
--- app/src/main/java/com/somestudents/steganographapp/MainActivity.java
+++ app/src/main/java/com/somestudents/steganographapp/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +18,14 @@ public class MainActivity extends AppCompatActivity {
 
         GridView gridView = (GridView) findViewById(R.id.gridView);
         gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(getApplicationContext(),FullImageActivity.class);
+                i.putExtra("id",position);
+                startActivity(i);
+            }
+        });
     }
 }
