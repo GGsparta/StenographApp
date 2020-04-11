aae1e8531e060f92d12645c7858b6eada63ee61e
Simon lauzeral <sim78100@hotmail.fr>
2020-04-11 18:55:50 -0400

    ajout de l'activite contenant l'image en grand

---
+++ app/src/main/java/com/somestudents/steganographapp/FullImageActivity.java
@@ -0,0 +1,48 @@
+package com.somestudents.steganographapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaTimestamp;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class FullImageActivity extends AppCompatActivity {
+
+    EditText editText;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_image);
+
+        Intent i = new Intent();
+        int position = i.getExtras().getInt("id");
+
+        ImageAdapter adapter = new ImageAdapter(this);
+
+        imageView = (ImageView) findViewById(R.id.fullImage);
+        imageView.setImageResource(adapter.images[position]);
+
+        editText = findViewById(R.id.edit_text);
+        editText.setText(GetEncryptedMessage());
+    }
+
+    public void SetMessage(View view){
+        SetEncryptedMessage(editText.getText().toString());
+        Intent i = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(i);
+    }
+
+
+
+    public void SetEncryptedMessage(String str){
+
+    }
+
+    public String GetEncryptedMessage(){
+        return "toto";
+    }
+}
