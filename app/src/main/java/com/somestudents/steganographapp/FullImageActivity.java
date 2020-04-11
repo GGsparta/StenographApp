a29fad6ef6526ce0abfd67f72cc5cec0e89a325b
Simon lauzeral <sim78100@hotmail.fr>
2020-04-11 19:27:25 -0400

    ajout de commentaire

--- app/src/main/java/com/somestudents/steganographapp/FullImageActivity.java
+++ app/src/main/java/com/somestudents/steganographapp/FullImageActivity.java
@@ -18,16 +18,24 @@ public class FullImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_image);
+
+        //on récupère l'intent venant de l'actrivité principale
         Intent i = getIntent();
+
+        //on récupère l'information de l'intent
         int position = i.getExtras().getInt("id");
+
+        //on affiche la bonne image dans l'ImageView
         ImageAdapter adapter = new ImageAdapter(this);
         imageView = (ImageView) findViewById(R.id.fullImage);
         imageView.setImageResource(adapter.images[position]);
 
+        //on recupère le texte de l'image et on l'écrit dans l'EditText
         editText = findViewById(R.id.edit_text);
         editText.setText(GetEncryptedMessage());
     }
 
+    /*cette fonction est appelé lorsque le bouton apply est appuyé*/
     public void SetMessage(View view){
         SetEncryptedMessage(editText.getText().toString());
         Intent i = new Intent(getApplicationContext(),MainActivity.class);
@@ -35,12 +43,14 @@ public class FullImageActivity extends AppCompatActivity {
     }
 
 
-
+    /*cette fonction sert à encrypter une string dans un image*/
     public void SetEncryptedMessage(String str){
-
+        //TODO
     }
 
+    /*cette fonction récupère un message encrypter dans une image*/
     public String GetEncryptedMessage(){
+        //TODO
         return "toto";
     }
 }
