405577e0509cba21253a5392315f5d4153572f91
Ambre <ambre_donnadieu@live.fr>
2020-04-12 00:20:57 +0200

    Affichage d'image dans une gridview depuis les drawable

--- app/src/main/java/com/somestudents/steganographapp/MainActivity.java
+++ app/src/main/java/com/somestudents/steganographapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.somestudents.steganographapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+
+        gridView.setAdapter(new ImageAdapter(this));
     }
 }
