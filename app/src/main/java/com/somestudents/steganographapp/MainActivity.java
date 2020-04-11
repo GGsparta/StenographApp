4524cdfc633438756521833ebff3b46ea5a6eb2d
Yoann HAFFNER <yoyofrom68@gmail.com>
2020-04-11 15:11:45 -0400

    Initialisation du projet

---
+++ app/src/main/java/com/somestudents/steganographapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.somestudents.steganographapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
